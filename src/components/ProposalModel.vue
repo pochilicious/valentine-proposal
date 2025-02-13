03092f3cacfac30ca3341547bec6f0752fd66658
Andrei Beniahan <beniahan321@gmail.com>
2025-02-13 18:17:02 +0800

    Updated ProposalModel.vue

--- src/components/ProposalModel.vue
+++ src/components/ProposalModel.vue
@@ -1,6 +1,6 @@
 ﻿<template>
     <div class="absolute top-1/4 left-1/2 w-screen flex flex-col text-center items-center transform -translate-x-1/2">
-        <h2 class="mb-6 mx-0 px-0 font-bold text-4xl">Wants to date me?</h2>
+        <h2 class="mb-6 mx-0 px-0 font-bold text-4xl"> Aking Sinta, ikaw ba ay maari kong ka valentine? :)</h2>
         <div>
             <button class="mr-10 bg-pink-500 text-white font-semibold py-2 px-4 rounded hover:bg-purple-700 transition duration-300 ease-in-out transform hover:scale-105" type="button" >YAS!</button>
             <button @mouseover="randomizePos" @click="randomizePos" :style="btnStyle" class="bg-red-500 text-white font-semibold py-2 px-4 rounded z-20" type="button">no</button>
